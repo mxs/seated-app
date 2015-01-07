ae7198c23197acf7bdb0baee71e76fd5f8770c3e
Michael Shang <michael.shang@gmail.com>
2015-01-08 00:45:27 +1100

    removed unnecessary import

--- seated/seated/seated-Bridging-Header.h
+++ seated/seated/seated-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 #import <Parse/Parse.h>
-#import <Parse/PFObject+Subclass.h>
 #import <Firebase/Firebase.h>
 #import "PTKView.h"
 #import <Stripe/Stripe.h>

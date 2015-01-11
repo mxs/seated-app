92ddcac0c727a4a843ee5422bac5daae0723c7c1
Michael Shang <michael.shang@gmail.com>
2015-01-11 14:43:38 +1100

    logout implemented from settings closes #12

--- seated/seated/SettingsViewControllerTableViewController.swift
+++ seated/seated/SettingsViewControllerTableViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
-class SettingsViewControllerTableViewController: UITableViewController {
+class SettingsViewControllerTableViewController: UITableViewController, UIAlertViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func logout(sender: AnyObject) {
+        let alertView = UIAlertView(title: "", message: "Are you sure?", delegate: self, cancelButtonTitle: "Cancel", otherButtonTitles: "Log out")
+        alertView.show()
+    }
+    
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        if buttonIndex == 1 {
+            PFUser.logOut()
+            self.performSegueWithIdentifier("logoutSegue", sender: self)
+        }
     }
 }

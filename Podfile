79f8b353e0e29a3b5d2c1cf1b54ad7a0eb88d5c1
Michael Shang <michael.shang@gmail.com>
2015-02-11 18:44:44 +1100

    updated pods

--- Podfile
+++ Podfile
@@ -2,7 +2,7 @@ xcodeproj 'seated/seated.xcodeproj'
 
 pod 'Stripe'
 pod 'PaymentKit'
-pod 'Parse'
+pod 'Parse', '~> 1.6'
 pod 'Firebase', '>= 2.1.0'
 pod 'JSQMessagesViewController'
 pod 'SVProgressHUD'
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -16,25 +16,25 @@
 #define COCOAPODS_POD_AVAILABLE_Firebase
 #define COCOAPODS_VERSION_MAJOR_Firebase 2
 #define COCOAPODS_VERSION_MINOR_Firebase 1
-#define COCOAPODS_VERSION_PATCH_Firebase 0
+#define COCOAPODS_VERSION_PATCH_Firebase 2
 
 // JSQMessagesViewController
 #define COCOAPODS_POD_AVAILABLE_JSQMessagesViewController
 #define COCOAPODS_VERSION_MAJOR_JSQMessagesViewController 6
 #define COCOAPODS_VERSION_MINOR_JSQMessagesViewController 1
-#define COCOAPODS_VERSION_PATCH_JSQMessagesViewController 1
+#define COCOAPODS_VERSION_PATCH_JSQMessagesViewController 3
 
 // JSQSystemSoundPlayer
 #define COCOAPODS_POD_AVAILABLE_JSQSystemSoundPlayer
 #define COCOAPODS_VERSION_MAJOR_JSQSystemSoundPlayer 2
 #define COCOAPODS_VERSION_MINOR_JSQSystemSoundPlayer 0
-#define COCOAPODS_VERSION_PATCH_JSQSystemSoundPlayer 0
+#define COCOAPODS_VERSION_PATCH_JSQSystemSoundPlayer 1
 
 // Parse
 #define COCOAPODS_POD_AVAILABLE_Parse
 #define COCOAPODS_VERSION_MAJOR_Parse 1
 #define COCOAPODS_VERSION_MINOR_Parse 6
-#define COCOAPODS_VERSION_PATCH_Parse 1
+#define COCOAPODS_VERSION_PATCH_Parse 2
 
 // PaymentKit
 #define COCOAPODS_POD_AVAILABLE_PaymentKit
@@ -51,12 +51,12 @@
 // Stripe
 #define COCOAPODS_POD_AVAILABLE_Stripe
 #define COCOAPODS_VERSION_MAJOR_Stripe 3
-#define COCOAPODS_VERSION_MINOR_Stripe 0
+#define COCOAPODS_VERSION_MINOR_Stripe 1
 #define COCOAPODS_VERSION_PATCH_Stripe 0
 
 // Stripe/Core
 #define COCOAPODS_POD_AVAILABLE_Stripe_Core
 #define COCOAPODS_VERSION_MAJOR_Stripe_Core 3
-#define COCOAPODS_VERSION_MINOR_Stripe_Core 0
+#define COCOAPODS_VERSION_MINOR_Stripe_Core 1
 #define COCOAPODS_VERSION_PATCH_Stripe_Core 0
 
